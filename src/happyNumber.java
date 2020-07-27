2a0ec854900c29073ab3ded3e13466a5db79d728
Anthony Mandala <anthonymmandala1@gmail.com>
2020-07-27 14:16:24 -0400

    fixed loop issue

--- src/happyNumber.java
+++ src/happyNumber.java
@@ -7,7 +7,7 @@ is happy if the sum of each digit squared is equal to 1. If result is 0, then th
  */
 
 import java.lang.reflect.Array;
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.regex.*;
 
@@ -15,15 +15,20 @@ public class happyNumber {
 
     public static void main(String[] args) {
         Scanner inputReader = new Scanner(System.in);
+        System.out.println("A happy number is a number where the sum of each digit squared is equal to 1. If the result loops back to the starting number");
+        System.out.println("then it is not a happy number.\n");
         System.out.println("Input a positive integer to check if it is happy or not:");
 
         int input = inputReader.nextInt();
         checkIfHappy(input);
-        
+
     }
 
     public static void checkIfHappy(int number) {
+        // set the first number for check later outside loop
         int firstNum = number;
+        ArrayList<Integer> checkedSums = new ArrayList();
+
         while(number != 1) { // run until happy or repeat
             String numString = Integer.toString(number);
             int[] digits = new int[numString.length()];
@@ -35,9 +40,10 @@ public class happyNumber {
             for (int j = 0; j < digits.length; j++) {
                 int numFromIndex = digits[j];
                 int squared = numFromIndex * numFromIndex;
+                System.out.println(digits[j] + "^2 is: " + squared);
                 digits[j] = squared;
             }
-            System.out.println(Arrays.toString(digits));
+            //System.out.println(Arrays.toString(digits));
 
             int sumOfDigits = 0;
             for(int k = 0; k < digits.length; k++) {
@@ -47,13 +53,15 @@ public class happyNumber {
             number = sumOfDigits;
             System.out.println("Sum of digits is: " + sumOfDigits);
 
-            if(sumOfDigits == firstNum) {
+
+            if(sumOfDigits == firstNum || checkedSums.contains(sumOfDigits)) {  // || sum of digits is in list already
                 System.out.println(firstNum + " is not happy");
                 break;
             } else if (sumOfDigits == 1) {
                 System.out.println(firstNum + " is happy");
                 number = 1;
             }
+            checkedSums.add(sumOfDigits);
         }
 
     }
